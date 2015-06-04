76e5c9e483307eef8b9ea5ca11861de0df961b97
aedanmac <aedanmac@gmail.com>
2015-06-03 19:37:46 -0700

    initial crud done, user authentication started

---
+++ db/migrate/20150604021149_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :name
+
+      t.index(:email, unique: true)
+
+      t.timestamps null: false
+    end
+  end
+end
